10c254577428f4cfbee1650c27e3e2e1b19a453d
Anand Kumar <anandkumar78402@gmail.com>
2024-10-17 16:21:13 +0530

    chnages

--- src/components/menu.js
+++ src/components/menu.js
@@ -11,27 +11,14 @@ export const menuItems = [
     icon: 'ri-user-fill',
     link: '/',
     userType: [1]
-  },
-  {
-    id: 2,
-    label: 'menuitems.dashboard.devices',
-    icon: 'fas fa-thermometer',
-    link: '/device',
-    userType: [1,2]
-  },
-  {
-    id: 3,
-    label: 'menuitems.dashboard.measurements',
-    icon: 'fas fa-ruler-horizontal',
-    link: '/measurements',
-    userType: [1,2]
-  },
-  {
-    id: 4,
-    label: 'menuitems.dashboard.settings',
-    icon: 'fas fa-wrench',
-    link: '/settings',
-    userType: [1]
   }
+  // {
+  //   id: 2,
+  //   label: 'menuitems.dashboard.devices',
+  //   icon: 'fas fa-thermometer',
+  //   link: '/device',
+  //   userType: [1,2]
+  // },
+ 
 ]
 
